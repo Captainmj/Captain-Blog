b3bbef06672a4f43e7960c74aa181fa9fe32f1b8
Captain_MJ <847727024@qq.com>
2017-05-17 21:33:39 +0800

    修改xadmin

--- Captian Blog/Web/settings.py
+++ Captian Blog/Web/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = [u'*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'haystack',
+    #'xadmin',
+    #'crispy_forms',
     'Blog',
     'user',
 ]
--- Captian Blog/Web/urls.py
+++ Captian Blog/Web/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+#import xadmin
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    #url(r'^xadmin/', xadmin.site.urls),
     url(r'^user/', include('user.urls', namespace='user')),
     url(r'', include('Blog.urls', namespace='Blog')),
 ]
